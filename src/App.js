32d62b0f609e82c80159223870afebdfecd6fb21
Kabakova <kabdia@mail.ru>
2023-06-07 19:21:55 +0500

    //easy counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+//используем хук useState для изменения состояния
+  let [number,setNumber]=useState(0)
+
+//функция для кнопки "Минус"
+ let clickMinus =()=>{
+    return setNumber(--number)
+  }
+//функция для кнопки "Плюс"
+  let clickPlus =()=>{
+    return setNumber(++number)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='main__window'>      
+        <div className='window_blockcounter'>
+        <h2 className='window_title'>Счетчик</h2>
+        <h1 className='window_counter'>{number}</h1>
+        <div className='window__button'>
+        <button className ='button_minus' onClick={clickMinus}>- Минус</button>
+        <button className='button_plus'onClick={clickPlus}>Плюс +</button>
+        </div>  
+        </div>                
+      
+      </div>      
     </div>
   );
 }
